9b69b112b0217d21edee1c4e92e1d7e885c5a4ea
Jesus <jesus.jg.medina@gmail.com>
2019-03-08 12:36:57 -0500

    help

--- app/controllers/api/tests_controller.rb
+++ app/controllers/api/tests_controller.rb
@@ -3,4 +3,15 @@ class Api::TestsController < ApplicationController
     @test =  Test.all
     render json: @test
   end
+  def create
+    test = Test.new(title: params[:title], description: params[:description])
+    test.subject = Subject.find_or_create_by(name: params[:subject])
+    test.save
+    counter = 0
+    params[:questions].each do |question|
+      test.questions << Question.create(title: question, answer: params[:answers][counter])
+      puts question
+      counter +=1
+    end
+  end
 end
